cf2129f7c5d6679e581e6d4e453015ee5e21100f
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-01-05 16:23:32 -0600

    have audioAnalysis object back. even if its just one, the property is called headers

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 
 function App() {
   const [bearer, setBearer] = useState("")
+  const [songData, setSongData] = useState("")
   useEffect(() => {
     const innerFunc = async () => {
       const bearerToken = await axios.get("/spotifyBearer").catch(err => console.error(err))
@@ -13,6 +14,22 @@ function App() {
     }
     innerFunc()
   }, [])
+  const clickHandler = async () => {
+    const songId = '2PpruBYCo4H7WOBJ7Q2EwM' // Hey Ya!
+    console.log(bearer)
+    const responseOK = await axios.get(`https://api.spotify.com/v1/audio-analysis/${songId}`, {
+      headers: {
+        Authorization: `Bearer ${bearer}`
+      }
+    }).catch(err => console.error(err))
+    // const songData = await axios.get(`https://api.spotify.com/v1/audio-analysis/${songId}`, {
+    //   header: {
+    //     Authorization: `Bearer ${bearer}`
+    //   }
+    // }).catch(err => console.error(err))
+
+    console.log(responseOK)
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -20,14 +37,12 @@ function App() {
         <p>
           if you see <code>{bearer}</code> the its time to get some data.
         </p>
-        <a
+        <div
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => clickHandler()}
         >
-          Learn React
-        </a>
+          Click for some audio analysis data
+        </div>
       </header>
     </div>
   );
